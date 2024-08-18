212800cabfa7f69d365ac3509b76734def9179e2
seanmaier <sean.maier@outlook.de>
2024-08-18 23:53:50 +0200

    Added Dark Mode toggle

--- game-hub/src/components/Navbar.tsx
+++ game-hub/src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch.tsx";
 
 const Navbar = () => {
     return <HStack>
         <Image src={logo} boxSize="60px"></Image>
+        <ColorModeSwitch></ColorModeSwitch>
     </HStack>
 }
 
